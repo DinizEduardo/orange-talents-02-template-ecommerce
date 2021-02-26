73cbc0671bb1f4f6fcf72a693d3009778240b691
DinizEduardo <eduardo.diniz987@gmail.com>
2021-02-26 12:28:39 -0300

    3-3-4-testes-automatizados-codigo-para-pagina-detalhe

--- src/test/java/br/com/zup/mercadolivre/mercadoLivre/controller/produto/ProdutoControllerTest.java
+++ src/test/java/br/com/zup/mercadolivre/mercadoLivre/controller/produto/ProdutoControllerTest.java
@@ -3,12 +3,15 @@ package br.com.zup.mercadolivre.mercadoLivre.controller.produto;
 import br.com.zup.mercadolivre.mercadoLivre.model.*;
 import br.com.zup.mercadolivre.mercadoLivre.model.request.CaracteristicaRequest;
 
+import static br.com.zup.mercadolivre.mercadoLivre.utils.CaracteristicasHelper.geraCaracteristicas;
 import static br.com.zup.mercadolivre.mercadoLivre.utils.CaracteristicasHelper.geraCaracteristicasRequest;
+import static br.com.zup.mercadolivre.mercadoLivre.utils.MockMvcRequest.performGet;
 import static br.com.zup.mercadolivre.mercadoLivre.utils.MockMvcRequest.performPost;
 import static org.junit.jupiter.api.Assertions.*;
 
 import br.com.zup.mercadolivre.mercadoLivre.model.request.ProdutoRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -53,6 +57,45 @@ public class ProdutoControllerTest {
 
         categoriaMae = new Categoria("testeCategoriaMae");
         manager.persist(categoriaMae);
+    }
+
+    @Test
+    @WithUserDetails("teste@logado.com")
+    public void deveriaPegarOsDetalhesDeUmProduto() throws Exception{
+        List<Caracteristica> caracteristicas = geraCaracteristicas(3);
+
+        Produto produto = new Produto("Mouse", 100, 5, caracteristicas,
+                "Um produto top", categoriaMae);
+
+        manager.persist(produto);
+
+        MvcResult result = performGet(mockMvc, "/produtos/" + produto.getId(), 200, objectMapper, null);
+
+        JSONObject json = new JSONObject(result.getResponse().getContentAsString());
+
+        assertAll(
+                () -> assertEquals(json.get("nome").toString(), produto.getNome()),
+                () -> assertEquals(Double.parseDouble(json.get("valor").toString()), produto.getValor()),
+                () -> assertEquals(Integer.parseInt(json.get("quantidade").toString()), produto.getQuantidade()),
+                () -> assertEquals(json.get("descricao").toString(), produto.getDescricao()),
+                () -> assertEquals(json.get("opinioes").toString(), "[]"),
+                () -> assertEquals(json.get("perguntas").toString(), "[]")
+
+        );
+
+    }
+
+    @Test
+    @WithUserDetails("teste@logado.com")
+    public void naoDeveriaPegarDetalhesDoProdutoComIdInvalido() throws Exception{
+        List<Caracteristica> caracteristicas = geraCaracteristicas(3);
+
+        Produto produto = new Produto("Mouse", 100, 5, caracteristicas,
+                "Um produto top", categoriaMae);
+
+        manager.persist(produto);
+
+        performGet(mockMvc, "/produtos/" + produto.getId() + 1, 404, objectMapper, null);
     }
 
     @Test
